ad3819140834c0362b6938a4552283d9c73bd2b2
Thomas Morrell <tmorrell@users.noreply.github.com>
2023-07-25 10:38:34 -0700

    contrib: add additional thesis fields

--- invenio_rdm_records/contrib/thesis/custom_fields.py
+++ invenio_rdm_records/contrib/thesis/custom_fields.py
@@ -9,6 +9,8 @@
 Implements the following fields:
 
 - thesis.university
+- thesis.department
+- thesis.type
 """
 from invenio_i18n import lazy_gettext as _
 from invenio_records_resources.services.custom_fields import TextCF
@@ -20,6 +22,8 @@ THESIS_NAMESPACE = {
 
 THESIS_CUSTOM_FIELDS = [
     TextCF(name="thesis:university"),
+    TextCF(name="thesis:department"),
+    TextCF(name="thesis:type")
 ]
 
 THESIS_CUSTOM_FIELDS_UI = {
@@ -35,6 +39,16 @@ THESIS_CUSTOM_FIELDS_UI = {
                     "label": _("Awarding university"),
                     "placeholder": "",
                     "description": "",
+                },
+                "department": {
+                    "label": _("Awarding department"),
+                    "placeholder": "",
+                    "description": "",
+                },
+                "type": {
+                    "label": _("Thesis type"),
+                    "placeholder": "PhD",
+                    "description": "The type of thesis (e.g. Masters, PhD, Engineers, Bachelors)",
                 },
             },
         }
